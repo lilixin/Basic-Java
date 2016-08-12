cab1fa7dd7a56b69a4b2734d98ab67a8677cc03a
lilixin <415613348@qq.com>
2016-08-12 17:26:01 +0800

    init

---
+++ src/top/lilixin/jvm/ReflectAndJVM.java
@@ -0,0 +1,18 @@
+/*
+ * @项目名称: Basic-Java
+ * @文件名称: ReflectAndJVM.java
+ * @Date: 2016-8-12
+ * @Copyright: 2016 www.lilixin.top Inc. All rights reserved.
+ * 注意：本内容仅限于内部传阅，禁止外泄以及用于其他的商业目的
+ */
+package top.lilixin.jvm;
+
+/**
+ * @Project: Basic-Java
+ * @Author: lilixin
+ * @Date: 2016年8月12日
+ * @Copyright: 2016 www.lilixin.top Inc. All rights reserved.
+ */
+public class ReflectAndJVM {
+
+}
